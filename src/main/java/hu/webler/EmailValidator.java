49a3ce41869a88cc0710910724057d02241e4afd
norba <norbaa79@gmail.com>
2024-03-06 15:39:09 +0100

    Create email validator using regex

---
+++ src/main/java/hu/webler/EmailValidator.java
@@ -0,0 +1,20 @@
+package hu.webler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+    private static final String EMAIL_VALIDATOR_REGEX =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_VALIDATOR_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    private EmailValidator() {
+
+    }
+}
